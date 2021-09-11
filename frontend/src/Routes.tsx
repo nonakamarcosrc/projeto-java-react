48cf6ac645320dd1c81f0045be46c9e0df72dd48
nonakamarcosrc <nonakamarcosrc@hotmail.com>
2021-09-11 07:56:04 -0300

    Home, navigation

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -11,7 +11,7 @@ const Routes = () => {
 
                 </Route>
 
-                <Route path="/Dashboard" exact>
+                <Route path="/dashboard" exact>
                     <Dashboard></Dashboard>
 
                 </Route>
